116cf27b381905535291e2802a20989a83843026
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-12 13:41:47 +0600

    add EndUserSetup
    Добавлена настройка wi-fi для настройки конечным пользователем.

---
+++ eus_params.lua
@@ -0,0 +1,10 @@
+local p={}
+p.wifi_ssid="rsbr23542"
+p.wifi_password="qq632145"
+p.mqtt_broker_ip="192.168.1.10"
+p.mqtt_broker_port="1883"
+p.mqtt_login="ha"
+p.mqtt_password="qq632145"
+p.mqtt_client_id="sownoff_backlight"
+p.mqtt_topic="/home/cabinet/"
+return p
